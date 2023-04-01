0b9592536bf2a20dce3184856f69a4a33bef059b
dejvokep <davidcubesvkdev@gmail.com>
2023-04-01 17:47:32 +0200

    Moved main Head component to the root app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import {Fragment} from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Fragment>
+    <Head>
+      <title>NAME</title>
+      <meta name="description" content="DESC" />
+      <link rel="icon" href="/favicon.png" />
+    </Head>
+    <Component {...pageProps} />
+  </Fragment>
 }
 
 export default MyApp
